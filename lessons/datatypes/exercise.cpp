de1ef42a488264e4de685f32b8f477d99dddd158
haroldmud <haroldmudosa@gmail.com>
2025-11-11 15:19:34 +0200

    feat: learning about datatypes

---
+++ lessons/datatypes/exercise.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 3;
+  float b = 4.5;
+  double c = 5.25;
+  double sum;
+	sum = a + b + c;
+
+  cout << "The sum of a, b, and c is " << sum << endl;
+  return 0;
+}
