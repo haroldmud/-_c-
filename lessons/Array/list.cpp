642d87693e594d6d3ae66f7c5f0b791b1c5aa607
haroldmud <haroldmudosa@gmail.com>
2025-11-12 16:06:42 +0200

    feat: implementing the array elements

---
+++ lessons/Array/list.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int intArr[5] = {1,2,3};
+  char charArr[5] = {'a', 'd'};
+	
+	// for(int i=0; i<5; i++) {
+	// 	cout << intArr[i] * intArr[3] << " ";
+	// };
+
+  // for(int i=0; i<5; i++) {
+	// 	cout << charArr[i] << " ";
+	// };
+ return 0;
+}
