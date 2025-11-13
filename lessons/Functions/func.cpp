70fee242fc493eeace54b675872b953983ae44cc
haroldmud <haroldmudosa@gmail.com>
2025-11-13 12:28:52 +0200

    feat: worked one an exercise

---
+++ lessons/Functions/func.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void callname(string name) {
+  cout << "Are you there: " + name << endl;
+}
+
+int main() {
+  callname("zainab");
+}
