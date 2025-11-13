9aea178666ceb8d4eff845bb11c5dff288efe888
haroldmud <haroldmudosa@gmail.com>
2025-11-13 11:03:02 +0200

    feat: work with strings and if else

---
+++ lessons/Strings/text.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string mytext = "Money over love";
+  cout << "the song I'm listening to is: " << mytext; 
+  return 0;
+}
