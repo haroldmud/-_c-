99f85c62145499e15e5680324042f2be3b152497
haroldmud <haroldmudosa@gmail.com>
2025-11-14 11:45:35 +0200

    feat: dealing with vectors

--- challenges/lowtohigh.cpp
+++ challenges/lowtohigh.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 using namespace std;
 
+// FOR ARRAYS
 int main() {
   int nums[] = {3,2,1,12,5};
   int n = sizeof(nums) / sizeof(nums[0]);
@@ -13,4 +14,17 @@ int main() {
   int res = sum - nums[0] - nums[n-1];
   cout << res;
   return 0;
+}
+// 10
+
+// FOR VECTORS
+
+int sum(vector<int> numbers)
+{
+    sort(numbers.begin(), numbers.end());
+    int sum = 0;
+    for(int x: numbers) {
+      sum += x;
+    }
+    return sum - numbers[0] - numbers[numbers.size() - 1];
 }
