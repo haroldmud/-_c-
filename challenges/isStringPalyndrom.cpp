4b605729db74bcd00dee26e4c0c95972972641bb
haroldmud <haroldmudosa@gmail.com>
2025-11-16 17:45:45 +0200

    feat: palyndrom challenge

--- challenges/isStringPalyndrom.cpp
+++ challenges/isStringPalyndrom.cpp
@@ -6,8 +6,8 @@ using namespace std;
 bool isPalindrom (const string& str)
 {
   string myString = str;
-  transform(str.begin(), str.end(), str.begin(), ::tolower());
-  transform(myString.begin(), myString.end(), myString.begin(), ::tolower());
+  transform(str.begin(), str.end(), str.begin(), ::tolower()); // convert str to lowercase by iterating through it all
+  transform(myString.begin(), myString.end(), myString.begin(), ::tolower()); // convert myString to lowercase
   reverse(myString.begin(), myString.end());
   return str == myString;
 }
