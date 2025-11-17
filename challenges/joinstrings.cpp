fd00e822cfed4781aabcf025394ffd81ddf22349
haroldmud <haroldmudosa@gmail.com>
2025-11-17 10:33:22 +0200

    feat: work on the strings joining

---
+++ challenges/joinstrings.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+string smash(const vector<string>& words)
+{
+  if(words.empty()) return ""; //when the space will be added, the output will  
+
+  string sentence = "";
+  vector<string> updated = words;
+  string last = words.back();
+  updated.pop_back();
+  
+  for(const string& n: updated) {
+    sentence += n + " ";
+  }
+  cout << sentence + last;
+
+  return sentence + last;
+}
+
+int main() {
+  smash(vector<string>{"bonjour", "paris"});
+  return 0;
+}
