f1521f6bbc5486f66fb741213623060f02521c8a
haroldmud <haroldmudosa@gmail.com>
2025-11-17 08:21:28 +0200

    feat: palyndrom challenge

--- challenges/isStringPalyndrom.cpp
+++ challenges/isStringPalyndrom.cpp
@@ -7,9 +7,11 @@ bool isPalindrom (const string& str)
 {
   string original = str;
   string reversed = str;
-  
+  // convert the existing strings to lowercase
   transform(original.begin(), original.end(), original.begin(), ::tolower);
   transform(reversed.begin(), reversed.end(), reversed.begin(), ::tolower);
+
+  // reverse the reversed variable's value
   reverse(reversed.begin(), reversed.end());
   return original == reversed; 
 }
