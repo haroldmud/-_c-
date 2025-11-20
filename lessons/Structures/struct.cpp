38c1cfdc6aab1b6d6936dfe92ef525073add217f
haroldmud <haroldmudosa@gmail.com>
2025-11-20 09:56:56 +0200

    feat: learning about structures

--- lessons/Structures/struct.cpp
+++ lessons/Structures/struct.cpp
@@ -12,16 +12,16 @@ int main (void){
 	person p2;
     
     
-    p1.name = "alice";
-    p1.age = 20;
-   	p1.do_programming = true;
+  p1.name = "alice";
+  p1.age = 20;
+ 	p1.do_programming = true;
 
-    p2.name = "bob";
-    p2.age = 18;
-    p2.do_programming = false;
+  p2.name = "bob";
+  p2.age = 18;
+  p2.do_programming = false;
 
-    cout << p1.name << " (" << p1.age << ")" << endl;
-    cout << p2.name << " (" << p2.age << ")" << endl;
+  cout << p1.name << " (" << p1.age << ")" << endl;
+  cout << p2.name << " (" << p2.age << ")" << endl;
 
-    return 0;
+  return 0;
 }
