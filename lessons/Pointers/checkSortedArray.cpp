5ef163b9b30e2690bbcee638fa26ab3bab244273
haroldmud <haroldmudosa@gmail.com>
2025-11-24 10:30:52 +0200

    feat: learning about the is_sorted method

---
+++ lessons/Pointers/checkSortedArray.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(const vector<int>& numbers) {
+  int *point_number = numbers;
+  cout << point_number << endl;
+  return 0;
+}
