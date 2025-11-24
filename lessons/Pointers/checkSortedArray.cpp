8297bc57046f50a8c61ce2ca347477f3b74b1b0d
haroldmud <haroldmudosa@gmail.com>
2025-11-24 10:40:22 +0200

    feat: learning about the is_sorted method

--- lessons/Pointers/checkSortedArray.cpp
+++ lessons/Pointers/checkSortedArray.cpp
@@ -1,10 +1,16 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <iostream>
 using namespace std;
 
 string is_sorted_and_how(const vector<int>& numbers) {
-  int *point_number = numbers;
-  
+  const int* point_number = numbers;
+  cout << point_number << endl;
+  return "noda";
 }
 
+int main() {
+  is_sorted_and_how({1, 1, 2, 3, 4, 5, 5});
+  return 0;
+}
