1347a926bd9c5a8e19e5d95adf5f6b62e543dd72
haroldmud <haroldmudosa@gmail.com>
2025-11-24 11:49:35 +0200

    feat: swapping function

---
+++ lessons/functionByReference/index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
+  cout << "a: " << a <<" b: " << b << endl;
+}
+
+int main() {
+  int x= 10, y =20;
+  swap(x, y);
+  return 0;
+}
+
